21ffdceca342809a6312a445a520e9fb3531544a
Adarsh1s <Adarsh79038@gmail.com>
2026-07-19 09:06:52 +0530

    Time: 3 ms (56.28%) | Memory: 25.7 MB (14.81%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,6 +1,29 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
+        unordered_set<char> rows[9],cols[9],boxes[9];
+        for(int i =0;i<9;i++){
+            for(int j =0;j<9;j++){
+                if(board[i][j]=='.') continue;
+
+                char digit = board[i][j];
+                int bidx = (i/3)*3+(j/3);
+                
+                if(rows[i].count(digit) || cols[j].count(digit)
+                || boxes[bidx].count(digit)) return false;
+
+                rows[i].insert(digit);
+                cols[j].insert(digit);
+                boxes[bidx].insert(digit);
+            }
+        }
+        return true;
+    }
+};
+
+/*class Solution {
+public:
+    bool isValidSudoku(vector<vector<char>>& board) {
         int s = 9;
         for(int j = 0;j<s;j++){
             unordered_map<int,int> map{{9,0}};
@@ -34,4 +57,4 @@ public:
         
         return true;
     }
-};
+};*/
