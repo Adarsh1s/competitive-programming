3fbbb262f17c6b9060370e0d6b1b9f69b11220e8
Adarsh Singh <Adarsh79038@gmail.com>
2026-08-16 01:05:51 +0530

    Time: 3 ms (25.33%) | Memory: 71.4 MB (5.05%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,21 +1,28 @@
+
 class Solution {
 public:
     int candy(vector<int>& ratings) {
-        int ans = 0;int n = ratings.size();
-        vector<int> temp(n,1);
-        //left pass
-        for(int i = 1;i<n;i++){
-            if(ratings[i-1]<ratings[i]) temp[i] = temp[i-1]+1;
-            ans += temp[i];
-        }
-        //right pass
-        for(int i = n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]){
-                ans -= temp[i];
-                temp[i] = max(temp[i+1]+1,temp[i]);
-                ans += temp[i];
+        int n = ratings.size();
+        vector<int> candies(n, 1);
+
+        // Left-to-right: enforce left-neighbor constraint
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
             }
         }
-        return ans+1;
+
+        // Right-to-left: enforce right-neighbor constraint
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
+            }
+        }
+
+        int total = 0;
+        for (int c : candies) {
+            total += c;
+        }
+        return total;
     }
 };
