290346bc046044e52c1a4221c5b7267717aba33e
Adarsh Singh <Adarsh79038@gmail.com>
2026-08-16 01:05:06 +0530

    Time: 11 ms (6.08%) | Memory: 71.2 MB (5.05%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -5,15 +5,17 @@ public:
         vector<int> temp(n,1);
         //left pass
         for(int i = 1;i<n;i++){
-            if(ratings[i-1]>=ratings[i]) continue;
-            else temp[i] = temp[i-1]+1;
+            if(ratings[i-1]<ratings[i]) temp[i] = temp[i-1]+1;
+            ans += temp[i];
         }
         //right pass
         for(int i = n-2;i>=0;i--){
-            if(ratings[i]>ratings[i+1]) 
-            temp[i] = max(temp[i+1]+1,temp[i]);
+            if(ratings[i]>ratings[i+1]){
+                ans -= temp[i];
+                temp[i] = max(temp[i+1]+1,temp[i]);
+                ans += temp[i];
+            }
         }
-        for(int i = 0;i<n;i++) ans += temp[i];
-        return ans;
+        return ans+1;
     }
 };
