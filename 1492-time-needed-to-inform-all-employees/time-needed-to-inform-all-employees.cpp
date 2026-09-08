6221d7f538848e1582a4d51d317b0e07a65b4bb9
Adarsh Singh <Adarsh79038@gmail.com>
2026-09-08 19:26:41 +0530

    Time: 92 ms (92.96%) | Memory: 116.5 MB (92.96%) - LeetSync

---
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int ans = INT_MIN;
+    void dfs(vector<int> adjList[],vector<int>& informTime,int curr,int time){
+        ans = max(ans,time);
+        for(int adjacent : adjList[curr]){
+            dfs(adjList,informTime,adjacent,time+informTime[curr]);
+        }
+    }
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        vector<int> adjList[n];
+
+        for(int i=0;i<n;i++){
+            if(manager[i] != -1){
+                adjList[manager[i]].push_back(i);
+            } 
+        }
+        dfs(adjList,informTime,headID,0);
+        return ans;
+    }
+};
