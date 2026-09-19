e250328a95dc0f7ccf31fe077884f36ea41bbb3e
Adarsh Singh <Adarsh79038@gmail.com>
2026-09-19 22:10:45 +0530

    Time: 0 ms (100.00%) | Memory: 17.4 MB (59.10%) - LeetSync

---
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool dfs(vector<vector<int>>& graph,vector<int>& color,int node,int col){
+        if(color[node] != 0) return (color[node]==col);
+        color[node] = col;
+        for(int ne: graph[node]){
+            if(!dfs(graph,color,ne,-col)) return false;
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        int  n = graph.size();
+        vector<int> color(n,0);
+
+        for(int node = 0;node<n; node++){
+            if(color[node]==0 && !dfs(graph,color,node,1)) return false;
+        }
+        return true;
+    }
+};
